3b612514f977479fe23191d88720ee95d44ca736
Roux S <roux@sudocode1.xyz>
2021-11-18 11:55:17 +0000

    add createcookie

--- dlib.js
+++ dlib.js
@@ -34,4 +34,14 @@ DLib.createBox = ({title, text, titleType='h3', textType='span', breakAfterTitle
 
     //return box;
 }
+
+
+/**
+ * @param {string} name - The name of the cookie
+ * @param {string} value - The cookie's value
+ * @param {number} expire - In how many seconds should the cookie expire
+ */
+DLib.createCookie = (name, value, expire=31536000) => {
+    document.cookie = `${name}=${value};secure;max-age=${expire};same-site=lax;`;
+};
 // @license-end
